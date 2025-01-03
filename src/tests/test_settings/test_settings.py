fc1d69a3ded08e35fbbfa11d208bfb97dd41f4e9
ASU10 <ASU10@email.com>
2025-01-03 02:44:40 -0500

    update

--- src/tests/test_settings/test_settings.py
+++ src/tests/test_settings/test_settings.py
@@ -46,58 +46,41 @@ ROOT_LOG_CONF = {
     }
 }
 
-# General assistant settings
-GENERAL_SETTINGS = {
-    'assistant_name': 'Jarvis',
-    'enabled_period': 300,  # In seconds
-    'commands_type': 'text',  # voice: The assistant waiting for voice commands,
-                                 # text: The assistant waiting for text commands
-    'response_in_speech': True,
-}
-
 # Google API Speech recognition settings
 # SpeechRecognition: https://pypi.org/project/SpeechRecognition/2.1.3
 SPEECH_RECOGNITION = {
     'ambient_duration': 1,  # Time for auto microphone calibration
-    'pause_threshold': 1,  # minimum length silence (in seconds) at the end of a sentence
-    'energy_threshold': 3000,  # microphone sensitivity, for loud places, the energy level should be up to 4000
-    'dynamic_energy_threshold': True  # For unpredictable noise levels (Suggested to be TRUE)
+    'pause_threshold':
+    1,  # minimum length silence (in seconds) at the end of a sentence
+    'energy_threshold':
+    3000,  # microphone sensitivity, for loud places, the energy level should be up to 4000
+    'dynamic_energy_threshold':
+    True  # For unpredictable noise levels (Suggested to be TRUE)
 }
 
 # SKill analyzer settings
 ANALYZER = {
     # SKill analyzer (TfidfVectorizer args)
     'args': {
-            "stop_words": None,
-            "lowercase": True,
-            "norm": 'l1',
-            "use_idf": False,
-            },
+        "stop_words": None,
+        "lowercase": True,
+        "norm": 'l1',
+        "use_idf": False,
+    },
     'sensitivity': 0.2,
-
 }
 
 # Google text to speech API settings
-GOOGLE_SPEECH = {
-    'lang': "en"
-}
-
+GOOGLE_SPEECH = {'lang': "en"}
 
 # Open weather map API settings
 # Create key: https://openweathermap.org/appid
-WEATHER_API = {
-    'unit': 'celsius',
-    'key': None
-}
+WEATHER_API = {'unit': 'celsius', 'key': None}
 
 # WolframAlpha API settings
 # Create key: https://developer.wolframalpha.com/portal/myapps/
-WOLFRAMALPHA_API = {
-    'key': None
-}
+WOLFRAMALPHA_API = {'key': None}
 
 # IPSTACK API settings
 #Create key: https://ipstack.com/signup/free
-IPSTACK_API = {
-    'key': None
-}
+IPSTACK_API = {'key': None}
